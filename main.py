6032b7f37d3ce117b7df9888476b29e4ebb60c26
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-27 21:28:53 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from flask import Flask, render_template, request
+
+# Data Collection
+# Use web scraping or publicly available datasets to gather real estate details
+# For demonstration purposes, we'll use an example dataset
+data = {
+    'location': ['Location A', 'Location B', 'Location C'],
+    'size': [1000, 1500, 2000],
+    'bedrooms': [3, 4, 5],
+    'bathrooms': [2, 3, 4],
+    'price': [500000, 750000, 1000000]
+}
+df = pd.DataFrame(data)
+
+# Data Preprocessing
+# Clean and transform the data
+def preprocess_data(df):
+    # Handle missing values
+    df = df.dropna()
+
+    # Outlier detection and removal (if applicable)
+    
+    # Feature engineering (if applicable)
+
+    return df
+
+df = preprocess_data(df)
+
+# Exploratory Data Analysis (EDA)
+def perform_eda(df):
+    # Visualize the distribution of house prices
+    sns.histplot(df['price'])
+    plt.title('House Price Distribution')
+    plt.xlabel('Price')
+    plt.ylabel('Frequency')
+    plt.show()
+
+    # Explore correlations between variables
+    sns.heatmap(df.corr(), annot=True)
+    plt.title('Correlation Heatmap')
+    plt.show()
+
+perform_eda(df)
+
+# Feature Selection
+def feature_selection(df):
+    # Perform feature selection using Recursive Feature Elimination or SelectKBest
+    # Select the most influential features for predicting house prices
+    # For demonstration purposes, we'll select all features
+    X = df[['size', 'bedrooms', 'bathrooms']]
+    y = df['price']
+    return X, y
+
+X, y = feature_selection(df)
+
+# Model Training
+def train_model(X, y):
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train a Linear Regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Train a Random Forest model
+    # model = RandomForestRegressor()
+    # model.fit(X_train, y_train)
+
+    return model, X_test, y_test
+
+model, X_test, y_test = train_model(X, y)
+
+# Model Evaluation
+def evaluate_model(model, X_test, y_test):
+    # Make predictions on the test set
+    y_pred = model.predict(X_test)
+
+    # Calculate evaluation metrics
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+
+    return mse, mae
+
+mse, mae = evaluate_model(model, X_test, y_test)
+print(f'Mean Squared Error: {mse}')
+print(f'Mean Absolute Error: {mae}')
+
+# User Interface
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def predict_price():
+    if request.method == 'POST':
+        size = int(request.form['size'])
+        bedrooms = int(request.form['bedrooms'])
+        bathrooms = int(request.form['bathrooms'])
+
+        # Preprocess the inputs (if necessary)
+        
+        # Make price prediction using the trained model
+        price = model.predict([[size, bedrooms, bathrooms]])
+
+        return render_template('result.html', price=price)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Deployment and Integration
+# Deploy the Flask web application as a web server for real-time predictions
+# Explore integration possibilities with existing real estate platforms
+# Further implementation details are required for successful deployment and integration.
